b9c8ae0a2af85cd6e0b6985161c6e9319a6e66ec
Farid <faridqasimov8@gmail.com>
2023-11-17 00:15:21 +0400

    update

--- src/components/Icons/Icons.js
+++ src/components/Icons/Icons.js
@@ -3,6 +3,7 @@ import classes from './Icons.module.css'
 import {MdPhoneInTalk} from 'react-icons/md';
 import {FaTelegramPlane,FaWhatsapp} from 'react-icons/fa'
 
+
 export default function Icons(props) {
   return (
     <div className={`${classes.icons} ${props.cl}`}>
